5b7f63e79174827fa1f5c98c8645697e2f24bf40
Paulo Ochoa <gersio769@gmail.com>
2022-02-24 18:49:27 -0400

    Revert "Revert "Another bug""
    This reverts commit 7e06a3338b9a59d602736ef8529d143914affa5e.

---
+++ Assets/Scripts/PowerButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerButton : MonoBehaviour
+{
+    public PlayerMovement Emperor;
+    public void powerSpeed()
+    {
+        Emperor.PowerSpeed();
+    }
+
+}
